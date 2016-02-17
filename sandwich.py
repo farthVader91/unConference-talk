3238ccbdbdfbf15b607c581409adf473ffdaa5df
Vishal Gowda <vishal@klonio.com>
2016-02-18 02:24:38 +0530

    Move choosing cheese to ingredients decorator

--- sandwich.py
+++ sandwich.py
@@ -5,35 +5,30 @@ def bread(func):
         print "<\______/>"
     return wrapper
 
-def layers(number=1, with_cheese=False):
+def layers(number=1):
     def wrap(func):
         def wrapped_f(*args, **kwargs):
-            kwargs.update({
-                'with_cheese': with_cheese
-                })
             for _ in range(number):
                 func(*args, **kwargs)
         return wrapped_f
     return wrap
 
-def ingredients(func):
-    def wrapper(*args, **kwargs):
-        print
-        with_cheese = kwargs.pop('with_cheese', False)
-        if with_cheese:
-            print "*cheese*"
-        print "#tomatoes#"
-
-        func(*args, **kwargs)
-        print "~salad~"
-        if with_cheese:
-            print "*cheese*"
-        print
-    return wrapper
+def ingredients(with_cheese=False):
+    def wrap(func):
+        def wrapper(*args, **kwargs):
+            print
+            print "#tomatoes#"
+            func(*args, **kwargs)
+            print "~salad~"
+            if with_cheese:
+                print "*cheese*"
+            print
+        return wrapper
+    return wrap
 
 @bread
-@layers(number=2, with_cheese=True)
-@ingredients
+@layers(number=2)
+@ingredients(with_cheese=True)
 def sandwich(food='--ham--'):
     print food
 
